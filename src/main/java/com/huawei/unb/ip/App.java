d8f5d4304378fbece356b5d4e0a6185f8b82a01e
KennyWang <kangwanga@126.com>
2016-09-14 02:46:10 +0800

    init project

---
+++ src/main/java/com/huawei/unb/ip/App.java
@@ -0,0 +1,13 @@
+package com.huawei.unb.ip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
